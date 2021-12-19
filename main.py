24a41c24bdf479dbb98a2cc336d2a171bda3f529
İbrahim Can Gençel <ibrahimcangencel@gmail.com>
2021-12-19 16:11:20 +0300

    2 SQL is added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from matplotlib import pyplot as plt
+from matplotlib.pyplot import figure
 from pyspark.sql import SparkSession
 from sql.sqls import SqlStrings
+import seaborn as sns
 
 # Initialize Spark
 spark = SparkSession.builder \
@@ -14,10 +16,17 @@ magazineDF.createOrReplaceTempView("magazine_table")
 
 # Calculating number of ratings per rating
 total_ratings = spark.sql(SqlStrings.groupby_rating_sql).toPandas()
-
 # Plotting number of ratings per rating
 total_ratings.plot(x="overall", y="count(1)", kind="bar")
 plt.xlabel('Rating')
 plt.ylabel('Number of Rating')
 plt.title('Number of Rating per Rating')
+plt.show()
+
+# Calculating number of ratings per rating and year
+total_ratings_year = spark.sql(SqlStrings.groupby_rating_time_sql).toPandas()
+figure(figsize=(20, 6), dpi=80)
+sns.barplot(x="year", y="rank_percentage", hue="overall", data=total_ratings_year)
+plt.ylabel('Number of Rating')
+plt.title('Number of Rating per Year and Rating')
 plt.show()
--- sql/sqls.py
+++ sql/sqls.py
@@ -3,4 +3,23 @@ class SqlStrings:
     SELECT overall, count(*) 
     FROM magazine_table
     GROUP BY overall
+    ORDER BY overall
+    """
+
+    groupby_rating_time_sql = """
+    WITH total_ratings_per_year as
+    (SELECT YEAR(FROM_UNIXTIME(unixReviewTime)) as year,COUNT(*) as count_of_total_reviews
+     FROM magazine_table
+     GROUP BY YEAR(FROM_UNIXTIME(unixReviewTime))
+     ORDER BY YEAR(FROM_UNIXTIME(unixReviewTime))
+    ),
+    total_ratings_per_year_w_ranks as
+    (SELECT YEAR(FROM_UNIXTIME(mt.unixReviewTime)) as year, mt.overall, count(*) as per_rank_year
+    FROM magazine_table as mt 
+    GROUP BY YEAR(FROM_UNIXTIME(mt.unixReviewTime)), mt.overall
+    ORDER BY YEAR(FROM_UNIXTIME(mt.unixReviewTime)), mt.overall
+    )
+    SELECT ty.year, tyr.overall, per_rank_year/count_of_total_reviews as rank_percentage 
+    FROM total_ratings_per_year as ty LEFT JOIN total_ratings_per_year_w_ranks tyr ON ty.year = tyr.year
+    ORDER BY ty.year, tyr.overall
     """
